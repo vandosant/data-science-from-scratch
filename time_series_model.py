bd7fd480fece789a0183d88ff71f372783b0cae9
vandosant <sjskender@gmail.com>
2016-04-12 12:03:03 -0600

    Create time series model

---
+++ time_series_model.py
@@ -0,0 +1,69 @@
+import sys
+
+months = []
+passengers = []
+mo = 1
+for line in sys.stdin.readlines():
+    if len(line.split()) > 1:
+        passengers.append(int(line.split()[1]))
+        months.append(mo)
+        mo+=1
+        if mo > 12:
+            mo = 1
+
+def predict(alpha, beta, x_i):
+    return beta * x_i + alpha
+
+def error(alpha, beta, x_i, y_i):
+    return y_i - predict(alpha, beta, x_i)
+
+def sum_of_squared_errors(alpha, beta, x, y):
+    return sum(error(alpha, beta, x_i, y_i)**2 for x_i, y_i in zip(x, y))
+
+def variance(x):
+    n = len(x)
+    deviations = de_mean(x)
+    return sum_of_squares(deviations) / (n - 1)
+
+def standard_deviation(x):
+    import math
+    return math.sqrt(variance(x))
+
+def correlation(x, y):
+    stdev_x = standard_deviation(x)
+    stdev_y = standard_deviation(y)
+    if stdev_x > 0 and stdev_y > 0:
+        return covariance(x, y) / stdev_x / stdev_y
+    else:
+        return 0
+
+def dot(v1, v2):
+    return sum(v1_i * v2_i for v1_i, v2_i in zip(v1, v2))
+
+def sum_of_squares(v):
+    return dot(v, v)
+
+def mean(x):
+    return sum(x) / len(x)
+
+def de_mean(x):
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
+
+def covariance(x, y):
+    n = len(x)
+    return dot(de_mean(x), de_mean(y)) / (n - 1)
+
+def least_squares_fit(x, y):
+    beta = correlation(x, y) * standard_deviation(y) / standard_deviation(x)
+    alpha = mean(y) - beta * mean(x)
+    return alpha, beta
+
+import matplotlib.pyplot as plt
+plt.scatter(months, passengers)
+plt.ylabel('some numbers')
+plt.show()
+
+alpha, beta = least_squares_fit(months, passengers)
+for x in range(1, 13):
+    print int(predict(alpha, beta, x))
